d28200d6ad4ac8f8c4c78192c38cfa1fa5f2d259
Christopher Stephens <cstep98@outlook.com>
2023-03-13 17:51:09 -0500

    added some code

--- RockPaperScissors/RPS.cs
+++ RockPaperScissors/RPS.cs
@@ -1,20 +1,48 @@
 ﻿namespace RockPaperScissors;
-class RPS_Base
+class RockPaperScissors
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HumanPlayer Human = new HumanPlayer(initPoints:0);
+        ComputerPlayer Machine = new ComputerPlayer(initPoints:0);
+        Console.WriteLine("Hello, World!"); 
     }
 }
 
 class HumanPlayer
 {
+private int points;
 
+public int Points {
+    get { return points; }
+    set { points = value;}
 }
+
+public HumanPlayer (int initPoints) {
+    this.Points = initPoints;
+}
+
+public int GetPoints() {
+    Console.WriteLine($"You have {this.Points} points");
+}
+public void WinRound()
+{
+    this.Points += 5;
+}
+public void LoseRound()
+{
+    this.Points -= 5;
+}
+public string HumanDecision(string humanChoice)
+{
+    Console.WriteLine("Input your desired shape: Rock, Paper, or Scissors:");
+    humanChoice = Console.ReadLine().toLower();
+    return humanChoice;
+}
+}
+
 
 class ComputerPlayer
 {
 
 }
-
-class RockPaperScissors

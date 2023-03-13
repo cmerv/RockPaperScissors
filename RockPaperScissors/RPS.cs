ce3f499bf8265dd2880ce58508be60d8c940633b
Christopher Stephens <cstep98@outlook.com>
2023-03-13 18:07:20 -0500

    added more features to computer and human classes

--- RockPaperScissors/RPS.cs
+++ RockPaperScissors/RPS.cs
@@ -3,15 +3,22 @@ class RockPaperScissors
 {
     static void Main(string[] args)
     {
-        HumanPlayer Human = new HumanPlayer(initPoints:0);
-        ComputerPlayer Machine = new ComputerPlayer(initPoints:0);
-        Console.WriteLine("Hello, World!"); 
+        HumanPlayer Human = new HumanPlayer(initPoints:5);
+        ComputerPlayer Machine = new ComputerPlayer();
+
+        while (Human.Points > 0)
+        {
+            ComputerPlayer.ComputerDecision();
+        }
+        Console.WriteLine("You now have 0 points and cannot continue. Thank you for playing. Game Over.");
     }
 }
 
 class HumanPlayer
 {
 private int points;
+
+public string humanChoice;
 
 public int Points {
     get { return points; }
@@ -33,16 +40,46 @@ public void LoseRound()
 {
     this.Points -= 5;
 }
-public string HumanDecision(string humanChoice)
+public string HumanDecision()
 {
     Console.WriteLine("Input your desired shape: Rock, Paper, or Scissors:");
+    humanChoice:
     humanChoice = Console.ReadLine().toLower();
-    return humanChoice;
+    switch (humanChoice)
+    {
+         default:
+            Console.WriteLine("You have input an invalid shape. Valid shape are: Rock, Paper, Scissors. Please try again:");
+            goto humanChoice;
+        case "rock":
+          return humanChoice;
+        case "paper":
+         return humanChoice;
+        case "scissors":
+         return humanChoice;
+    }
 }
 }
 
 
 class ComputerPlayer
 {
-
+    public class Random{}
+    public string ComputerChoice;
+    public string ComputerDecision()
+    {
+        var randObj = new Random();
+        int lowerLimit = 1;
+        int upperLimit = 3;
+        var randNum = Random.Next(lowerLimit, upperLimit);
+        switch (randNum)
+        {
+            case 1:
+            ComputerChoice = "rock";
+            case 2:
+            ComputerChoice = "paper";
+            case 3:
+            ComputerChoice = "scissors";
+        }
+        return ComputerChoice;
+    }
 }

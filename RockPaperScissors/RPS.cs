6e7664ff18683a1d9aa95e142e1935f2bcfd3373
Christopher Stephens <cstep98@outlook.com>
2023-03-13 17:27:06 -0500

    Initial

---
+++ RockPaperScissors/RPS.cs
@@ -0,0 +1,20 @@
+﻿namespace RockPaperScissors;
+class RPS_Base
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class HumanPlayer
+{
+
+}
+
+class ComputerPlayer
+{
+
+}
+
+class RockPaperScissors

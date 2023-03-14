4428f74d3a442bd7238cad4208dbfa12b288d18c
Christopher Stephens <cstep98@outlook.com>
2023-03-14 15:25:17 -0500

    Program complete.

--- RockPaperScissors/RPS.cs
+++ RockPaperScissors/RPS.cs
@@ -3,19 +3,22 @@ class RockPaperScissors
 {
     static void Main(string[] args)
     {
-        HumanPlayer Human = new HumanPlayer(initPoints:5);
-        ComputerPlayer Machine = new ComputerPlayer();
-    do {
-            Human.humanContinueOrNot=null;
+        HumanPlayer Human = new HumanPlayer(initPoints:5); // declaration of our objects
+        ComputerPlayer Machine = new ComputerPlayer(); 
+        PlayGame();
+
+    void PlayGame() // use of a method for separating the object instantiation from the bulk of the code
+    { do {
+            Human.humanContinueOrNot=null; // reset status of whether we want to continue, every time loop runs so that we are re-prompted.
             Human.HumanDecision();
             Machine.ComputerDecision();
             string machineChoice=Machine.computerChoice;
             string humanChoice=Human.humanChoice;
             Console.WriteLine($"Computer Decision:{machineChoice}");
             Console.WriteLine($"Your Decision:{humanChoice}");
-            if (machineChoice!=humanChoice)
+            if (machineChoice!=humanChoice) // efficient way to cut down on if statement usage. kick any equal hands out to the exception.
             {
-                switch (humanChoice)
+                switch (humanChoice) // meat of the comparison. only compares two thing: current hand, and hands that are not equal to it
                 {
                     case "rock":
                         if (machineChoice=="scissors")
@@ -54,21 +57,31 @@ class RockPaperScissors
             }
             else if (machineChoice==humanChoice)
             {  
+                switch (humanChoice){
+                 default:
                 Console.WriteLine("It's a tie.");
-                continue;
+                break;
+                }
             }
             do {
+            Human.GetPoints(); // display points
+            if (Human.Points > 0){ // prevent infinite loop
             Console.WriteLine("Play again? Input 'y' to continue or 'n' to exit:");
             Human.humanContinueOrNot = Console.ReadLine().ToLower();
             continue;
+            }
+            else if (Human.Points == 0)
+            {
+                break;
+            }
             } while (Human.humanContinueOrNot!="y"&&Human.humanContinueOrNot!="n");
     } while (Human.humanContinueOrNot!="n"&&Human.Points > 0);
         if (Human.Points==0)
         {
         Console.WriteLine("You now have 0 points and cannot continue. Thank you for playing. Game Over.");
         }
-
-        void HumanWinLose(bool humanWin)
+    }
+        void HumanWinLose(bool humanWin) // use of this method cleans up the code in the comparison section
         {
             if (humanWin==true)
             {
@@ -119,35 +132,38 @@ public string HumanDecision()
     humanChoice = null;
     humanChoice = Console.ReadLine();
     humanChoice = humanChoice.ToLower();
-    do {
-    switch (humanChoice)
+    
+    while (humanChoice!="rock"||humanChoice!="paper"||humanChoice!="scissors")
     {
-        default:
-        Console.WriteLine("You have input an invalid shape. Valid shape are: Rock, Paper, Scissors. Please try again:");
-        Console.WriteLine("Input your desired shape: Rock, Paper, or Scissors:");
-        humanChoice = Console.ReadLine();
-        return humanChoice; 
-        case "rock":
-         return humanChoice;
-        case "paper":
-         return humanChoice;
-        case "scissors":
-         return humanChoice;
+        switch (humanChoice)
+        {
+            default:
+            Console.WriteLine("You have input an invalid shape. Please try again.");
+            Console.WriteLine("Input your desired shape: Rock, Paper, or Scissors:");
+            humanChoice = Console.ReadLine();
+            break;
+            case "rock":
+            return humanChoice;
+            case "paper":
+            return humanChoice;
+            case "scissors":
+            return humanChoice;
+        }
     }
-    } while (humanChoice!="rock"||humanChoice!="paper"||humanChoice!="scissors");
+    return humanChoice; // this is in place to resolve an error involving no return. it has no use otherwise
+    
 }
 }
 
 class ComputerPlayer
 {
     public string computerChoice;
-    public string ComputerDecision()
+    public string ComputerDecision() // computer uses random numbers to determine
     {
         var randObj = new Random();
-        int lowerLimit = 1;
-        int upperLimit = 3;
-        var randNum = randObj.Next(lowerLimit, upperLimit);
-        switch (randNum)
+        var randNum = randObj.Next(1,4); // upper bound is exclusive
+        Console.WriteLine(randNum);
+        switch (randNum) // setting computerChoice based on numerals
         {
             case 1:
             computerChoice = "rock";
